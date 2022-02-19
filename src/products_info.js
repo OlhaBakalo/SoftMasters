7c9cc2b63f6d4bece42804a02543696594dd0b09
Yurii Denkovych <yd2004@ukr.net>
2022-02-19 17:49:35 +0200

    Update products_info.js

--- src/products_info.js
+++ src/products_info.js
@@ -12,3 +12,34 @@
     refs.modal.classList.toggle('products__info--ishidden');
   }
 })();
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[cream-info-open]'),
+    closeModalBtn: document.querySelector('[cream-info-close]'),
+    modal: document.querySelector('[cream-info]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('cream__info--ishidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[milk-info-open]'),
+    closeModalBtn: document.querySelector('[milk-info-close]'),
+    modal: document.querySelector('[milk-info]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('milk__info--ishidden');
+  }
+})();
